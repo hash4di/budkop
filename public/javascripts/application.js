195a7544babed3f1b2d6ecb11e53cca8a8279b18
Adrian <hash4di@gmail.com>
2010-12-10 16:00:09 +0100

    first try complite

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,115 @@
 $(function(){
-  $('#ex1').litebox('litebox');
+  $('#ex0').litebox({
+          target : 'litebox0',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex1').litebox({
+          target : 'litebox1',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex2').litebox({
+          target : 'litebox2',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex3').litebox({
+          target : 'litebox3',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex4').litebox({
+          target : 'litebox4',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex5').litebox({
+          target : 'litebox5',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex6').litebox({
+          target : 'litebox6',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex7').litebox({
+          target : 'litebox7',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex8').litebox({
+          target : 'litebox8',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex9').litebox({
+          target : 'litebox9',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex10').litebox({
+          target : 'litebox10',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex11').litebox({
+          target : 'litebox11',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex12').litebox({
+          target : 'litebox12',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
+  $('#ex13').litebox({
+          target : 'litebox13',
+          overlayBg : '111',
+          overlayOpacity : '1.5',
+	        liteboxShowSpeed : 800,
+	        close : 'close',
+	        overlayHideSpeed : 100
+  });
 });
 
