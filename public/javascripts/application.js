265dfd5015dd0eafe5eb13643969908b2cab611d
Adrian <hash4di@gmail.com>
2010-12-10 11:28:02 +0100

    official template

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,21 +1,4 @@
-var Application = {
-
-  setupTooltip: function() {
-    $('.tip').tooltip({
-      position: 'top center',
-      offset: [9, 0],
-      effect: 'fade',
-      opacity: 0.95,
-      tip: '.tooltip'
-    })
-  },
-
-  setup: function() {
-    this.setupTooltip()
-  }
-
-};
-
 $(function(){
-  Application.setup()
+  $('#ex1').litebox('litebox');
 });
+
