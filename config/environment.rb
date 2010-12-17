f5b710ae2c38a38d282d84816ccbbc2c2f52f30b
Adrian <hash4di@gmail.com>
2010-12-17 15:03:41 +0100

    haml to html

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,5 @@
 require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 BudKop::Application.initialize!
-Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'tmp/stylesheets' }
+
 
